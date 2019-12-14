e9eb373c3dea6578414f88ce49d6f2b61f5caeb3
Steven Chen <schenn1992@gmail.com>
2019-12-13 21:35:55 -0500

    add main func

--- products.py
+++ products.py
@@ -1,43 +1,53 @@
 
 import os # operating system 
 
-
-products = []
-
-if os.path.isfile('products.csv'):
-    print('yes! file found')
-    
-    with open('products.csv','r', encoding = 'utf-8') as f:
-        
-        for line in f:
+#read file
+def read_file(filename):
+    products = []
+    with open(filename, 'r', encoding = 'utf-8') as f:
+         for line in f:
             if 'product, price' in line:
                 continue
             name, price = line.strip().split(',')
             products.append([name, price])
-    
-    print(products)
-
-else:
-    print('file not found')
-
-
+    return products
 
 # allow user input
-while True:
-    name = input("Enter product name -> ")
-    if name == 'q':
-    	print('Have a nice day')
-    	break
-    price = input("Enter a price -> ")
-    products.append([name, price])
-print(products)
-
+def user_input(products):
+    while True: 
+        name = input("Enter product name -> ")
+        if name == 'q':
+            break
+        price = input("Enter a price -> ")
+        price = int(price)
+        products.append([name, price])
+    print(products)
+    return products
+ 
 #print out all record
-for product in products:
-	print(product[0], 'price is ->', product[1])
+def print_products(products):
+    for p in products:
+        print(p[0], 'price is ->', p[1])
 
+    
 #write in file
-with open('products.csv', 'w', encoding = 'utf-8') as f:
-	f.write('product,price\n') 
-	for product in products:
-		f.write(product[0] + ',' + product[1] + '\n')
+def write_file(filename, products):
+    with open(filename, 'w', encoding = 'utf-8') as f:
+        f.write('product, price\n') 
+        for p in products: 
+            f.write(p[0] + ',' + str(p[1]) + '\n')
+
+
+def main():
+    filename = 'products.csv'
+    if os.path.isfile(filename):
+        print('yes! file found')
+        products = read_file(filename)
+    else:
+        print('file not found')
+
+    products = user_input(products)
+    print_products(products)
+    write_file('products.csv', products)
+
+main()
